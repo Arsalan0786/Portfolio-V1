5d6dd4c5f4e7c693ac77045d9505a4da238d5213
Sheikh Arsalan <125729616+Arsalan0786@users.noreply.github.com>
2025-07-29 23:21:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: ''/portfolio-v1/'', // 👈 replace this!
   plugins: [react()],
   server: {
     port: 3000,
