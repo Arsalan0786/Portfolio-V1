f6ad18bbc71603dacc9756764416bfb50ca21109
Sheikh Arsalan <125729616+Arsalan0786@users.noreply.github.com>
2025-07-29 23:31:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Replace 'portfolio-v1' with your actual GitHub repo name if different
 export default defineConfig({
-  base: '/Portfolio-V1/',
+  base: '/Portfolio-V1/', // 👈 EXACTLY like this, case-sensitive
   plugins: [react()],
   server: {
     port: 3000,
